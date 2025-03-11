93d3fe34761e5b003c24fdb40e54a14156cb34c3
EddieExe <adityashriramjwar4384@gmail.com>
2025-03-11 21:55:34 +0530

    name for every video has been updated!

--- src/utils/dummyData.js
+++ src/utils/dummyData.js
@@ -3,7 +3,7 @@ export const dataInfo=[{
     imageIcon:'https://i.ytimg.com/vi/OPKpmCCyO-U/hq720.jpg?sqp=-oaymwEjCOgCEMoBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLBW0_HV1zj6SA50OP03hT4hZEP9Rw',
     video_url:"https://www.youtube.com/embed/OPKpmCCyO-U?si=G-BxVM2eoQ_msTdb",
     description:'Umbridge In The Forbidden Forest - Harry Potter And The Order Of The Phoenix',
-    owner:'Aman Singh',
+    owner:'Álex Pina',
     views:'11.2M Views',
     time:'4 years ago',
     genre:'entertainment'
@@ -13,7 +13,7 @@ export const dataInfo=[{
     imageIcon:'https://i.ytimg.com/vi/02pr2W7FT-c/hqdefault.jpg?sqp=-oaymwEjCPYBEIoBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLDnde6MIED4ku98otLz3AGjJZxmxQ',
     video_url:"https://www.youtube.com/embed/02pr2W7FT-c?si=dYX5ILLMpBB3S2FK",
     description:'Wizard Battle: Dumbledore vs Voldemort | Harry Potter and the Order of the Phoenix',
-    owner:'Aman Singh',
+    owner:'Álex Pina',
     views:'10.2M Views',
     time:'6 years ago',
     genre:'entertainment'
@@ -24,7 +24,7 @@ export const dataInfo=[{
     imageIcon:'https://i.ytimg.com/vi/Jy2_J5WCzDY/hqdefault.jpg?sqp=-oaymwEnCPYBEIoBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLBZ65JP67OFdblwswh1UWviQHJ-PQ',
     video_url:"https://www.youtube.com/embed/Jy2_J5WCzDY?si=4q6Wjgw-XxfeJ7L0",
     description:'Kung Fu Panda (2008) - Our Battle Will Be Legendary! Scene (7/10) | Movieclips',
-    owner:'Aman Singh',
+    owner:'Álex Pina',
     views:'7.2M Views',
     time:'10 years ago',
     genre:'entertainment'
@@ -35,7 +35,7 @@ export const dataInfo=[{
     imageIcon:'https://i.ytimg.com/vi/Oo3qsxihXqY/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLBbZ-EDOv9NehfJDvjc4MiQu0uxIQ',
     video_url:"https://www.youtube.com/embed/zKd_kxcxGbo?si=UcAq_HtV5IYfNY4V",
     description:'Mediocrity is Rising | Raw and real talk before 2024 ends.',
-    owner:'Aman Singh',
+    owner:'Álex Pina',
     views:'1.2M Views',
     time:'2 days ago',
     genre:'education'
@@ -45,7 +45,7 @@ export const dataInfo=[{
     imageIcon:'https://i.ytimg.com/vi/A2OQ-f_LOHA/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLB0SuqXNVwZASSmbuqDliGW-e_dZw',
     video_url:"https://www.youtube.com/embed/A2OQ-f_LOHA?si=x1s0p7w7B2VFWdun",
     description:'Emergency | Official Trailer | In Cinemas 17th January, 2025 | Kangana Ranaut',
-    owner:'Aman Singh',
+    owner:'Álex Pina',
     views:'30.2M Views',
     time:'2 months ago',
     genre:'entertainment'
@@ -56,7 +56,7 @@ export const dataInfo=[{
     imageIcon:'https://i.ytimg.com/vi/XK8loB2jYDE/hqdefault.jpg?sqp=-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLAOkgc-ZFjQ6pyu4ect1kLSWzPw0A',
     video_url:"https://www.youtube.com/embed/XK8loB2jYDE?si=VSA1UITL5ebIpVNd",
     description:'Reference Types: Objects && Arrays in JS || JavaScript Series 2024',
-    owner:'Aman Singh',
+    owner:'Álex Pina',
     views:'100K Views',
     time:'3 months ago',
     genre:'education'
@@ -68,7 +68,7 @@ export const dataInfo=[{
     imageIcon:'https://i.ytimg.com/vi/hP5s45CRFP8/hqdefault.jpg?sqp=-oaymwEmCKgBEF5IWvKriqkDGQgBFQAAiEIYAdgBAeIBCggYEAIYBjgBQAE=&rs=AOn4CLDstQvVdf-8Oosji6uwJQ5t1hCx6A',
     video_url:"https://www.youtube.com/embed/hP5s45CRFP8?si=PV2vW4mgl-P4_0qS",
     description:'TechXcelerate 24 - Day 4: Blockchain with Kanishk Khurana',
-    owner:'Aman Singh',
+    owner:'Álex Pina',
     views:'140K Views',
     time:'3 months ago',
     genre:'education'
@@ -80,7 +80,7 @@ export const dataInfo=[{
     imageIcon:'https://i.ytimg.com/vi/C9cN_6_qloA/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLAKsoIV3R5Wm8Hr56bEgmtE7_vReg',
     video_url:"https://www.youtube.com/embed/YBp2OcI-nGs?si=VDiYaxgJ_eTZ8rCa",
     description:'Vijayi Bhava - Full Video | Manikarnika | Kangana Ranaut | Shankar Ehsaan Loy | Prasoon Joshi',
-    owner:'Aman Singh',
+    owner:'Álex Pina',
     views:'140M Views',
     time:'3 years ago',
     genre:'songs'
@@ -90,7 +90,7 @@ export const dataInfo=[{
     imageIcon:'https://i.ytimg.com/vi/abyQXe3hxRc/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLA8gS6Qw4eFL2PR_XTCGVlgpVq0Tw',
     video_url:"https://www.youtube.com/embed/Su1LOpjvdZ4?si=y67ZoAa6sqHJKB3t",
     description:'Harry Potter and the Deathly Hallows - Main Trailer',
-    owner:'Aman Singh',
+    owner:'Álex Pina',
     views:'10M Views',
     time:'18 years ago',
     genre:'entertainment'
@@ -100,7 +100,7 @@ export const dataInfo=[{
     imageIcon:'https://i.ytimg.com/vi/0BDt08x-De8/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDDQXxRKSX3XZCKOrtZruCYYtIvpw',
     video_url:"https://www.youtube.com/embed/0BDt08x-De8?si=wemoyrN4_7F-YY_h",
     description:'E-Commerce clone will not get you hired - Real world project ideas',
-    owner:'Aman Singh',
+    owner:'Álex Pina',
     views:'1M Views',
     time:'1 years ago',
     genre:'education'
@@ -110,7 +110,7 @@ export const dataInfo=[{
     imageIcon:'https://i.ytimg.com/vi/VTLCoHnyACE/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLC8fQQIljrUqn0NzkqX1iiAdw9Dfg',
     video_url:"https://www.youtube.com/embed/1yrh60og6qc?si=rjJ6towYvc7NQmPz",
     description:'DSA Complete Series 🚀 : Data Structures & Algorithms | by Shradha Khapra Ma"am',
-    owner:'Aman Singh',
+    owner:'Álex Pina',
     views:'1M Views',
     time:'1 years ago',
     genre:'education'
@@ -120,7 +120,7 @@ export const dataInfo=[{
     imageIcon:'https://i.ytimg.com/vi/zxLH5aSmlBI/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLBKeyx4tFQY5TBO5SJ5_W23sEtK7g',
     video_url:"https://www.youtube.com/embed/VA0mJHo5X_s?si=NcNUGoXSoDPBFj0_",
     description:'Kamla Harris Says she will Use Everything if Ukraine Gets Nukes | Vantage With Palki Sharma',
-    owner:'Aman Singh',
+    owner:'Álex Pina',
     views:'1M Views',
     time:'1 years ago',
     genre:'news'
@@ -131,7 +131,7 @@ export const dataInfo=[{
     imageIcon:'https://i.ytimg.com/vi/Z9T3aiIsfx8/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLCwTPv1ubN26vY42yHpqq4BAlyfTw',
     video_url:"https://www.youtube.com/embed/Z9T3aiIsfx8?si=Woi6pJoU38sFrAlj",
     description:'How CANADA is becoming NEXT PAKISTAN | Abhi and Niyu',
-    owner:'Aman Singh',
+    owner:'Álex Pina',
     views:'4M Views',
     time:'12 days ago',
     genre:'news'
@@ -142,7 +142,7 @@ export const dataInfo=[{
     imageIcon:'https://i.ytimg.com/vi/Ez8F0nW6S-w/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLA6fPCgrA7WtxyXnkmP8p92K569Vw',
     video_url:"https://www.youtube.com/embed/sPM2WiwA1us?si=17Dg1Vs0wtC2mvjw",
     description:'GSoC 2025 Complete Roadmap | Google Summer of Code',
-    owner:'Aman Singh',
+    owner:'Álex Pina',
     views:'1M Views',
     time:'1 years ago',
     genre:'education'
@@ -153,7 +153,7 @@ export const dataInfo=[{
     imageIcon:'https://i.ytimg.com/vi/H_dhWRxR2rI/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLBPCzS7kL50GA1rogFafhWHfdTDVg',
     video_url:"https://www.youtube.com/embed/H_dhWRxR2rI?si=MsnSk_GThjOZWfGe",
     description:'Aandhi | Article 370 | Yami Gautam | Shashwat Sachdev | Clinton Cerejo | Shahzad Ali',
-    owner:'Aman Singh',
+    owner:'Álex Pina',
     views:'1M Views',
     time:'1 years ago',
     genre:'songs'
@@ -163,7 +163,7 @@ export const dataInfo=[{
     imageIcon:'https://i.ytimg.com/vi/60ehXa-s-AA/hq720.jpg?sqp=-oaymwEnCOgCEMoBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLCuYifD7mwl8K64frCBhfwmQ3cS4A',
     video_url:"https://www.youtube.com/embed/60ehXa-s-AA?si=Ag_czCxV76pO_H4t",
     description:'Chefling के owner ने pitch से पहले Sharks को दी एक दिलचस्प रिश्वत | Mouth-watering Pitches',
-    owner:'Aman Singh',
+    owner:'Álex Pina',
     views:'1.4M Views',
     time:'1 years ago',
     genre:'entertainment'
@@ -173,7 +173,7 @@ export const dataInfo=[{
     imageIcon:'https://i.ytimg.com/vi/dKYFMa0x9Rg/hq720.jpg?sqp=-oaymwEnCOgCEMoBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLCl9sIZGs2pNRo87eULwAL_-sjB2A',
     video_url:"https://www.youtube.com/embed/dKYFMa0x9Rg?si=qEjNi_Z6LohZ45jj",
     description:'TVFs Kota Factory | Exam Anthem | Lets Crack it!',
-    owner:'Aman Singh',
+    owner:'Álex Pina',
     views:'1.9M Views',
     time:'2 years ago',
     genre:'songs'
@@ -183,7 +183,7 @@ export const dataInfo=[{
     imageIcon:"https://i.ytimg.com/vi/7YhpvbVWm4c/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLAwwMA5zZfesLiu5LOFVwiWKFU_kg",
     video_url:"https://www.youtube.com/embed/SIlfq9356lU?si=VOpAYqmIXBHNdeqm",
     description:'Obliviate',
-    owner:'Aman Singh',
+    owner:'Álex Pina',
     views:'3.9M Views',
     time:'12 years ago',
     genre:'songs'
@@ -195,7 +195,7 @@ export const dataInfo=[{
     imageIcon:'https://i.ytimg.com/vi/VYUL1R4pupU/hq720.jpg?sqp=-oaymwFBCNAFEJQDSFryq4qpAzMIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB8AEB-AH-CYAC0AWKAgwIABABGEEgTShlMA8=&rs=AOn4CLCA9UkFy333e_Nkz9dNI2lEsAG6kQ',
     video_url:"https://www.youtube.com/embed/VYUL1R4pupU?si=KqpElpK8CWWNpfRX",
     description:'Hamas leader Yahya Sinwar׳s exclusive interview on BBC',
-    owner:'Aman Singh',
+    owner:'Álex Pina',
     views:'3.4M Views',
     time:'1 years ago',
     genre:'comedy'
@@ -205,7 +205,7 @@ export const dataInfo=[{
     imageIcon:'https://i.ytimg.com/vi/F-UDrbMfqTs/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLBBgTwAQkleDT4SA97aPfh79PCK1w',
     video_url:"https://www.youtube.com/embed/F-UDrbMfqTs?si=qH7dCQETZvWr0ly1",
     description:'Harry Potter And The Goblet of Fire | Death Eaters at the World Cup | ClipZone: Villains & Heroes',
-    owner:'Aman Singh',
+    owner:'Álex Pina',
     views:'5.9M Views',
     time:'12 years ago',
     genre:'entertainment'
@@ -215,7 +215,7 @@ export const dataInfo=[{
     imageIcon:'https://i.ytimg.com/vi/rW_fwcmyIfk/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLAQXjSS2SttMZHILPw964_jzfwHgw',
     video_url:"https://www.youtube.com/embed/ke8b60x_IpQ?si=UnOPY2HzI7ZaW2bh",
     description:'🇮🇳🥇 Neeraj Chopra wins historic gold for India | #Tokyo2020 Highlights',
-    owner:'Aman Singh',
+    owner:'Álex Pina',
     views:'5.9M Views',
     time:'12 years ago',
     genre:'sports'
@@ -225,7 +225,7 @@ export const dataInfo=[{
     imageIcon:'https://i.ytimg.com/vi/baLojo56L5M/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLCIzG-vtSJcQ6y2LJ6ouQnaKRCgbw',
     video_url:"https://www.youtube.com/embed/baLojo56L5M?si=2gJ7z-Fd3R4_emF_",
     description:'Anti-Islamist Comedian DESTROYS Hamas',
-    owner:'Aman Singh',
+    owner:'Álex Pina',
     views:'5M Views',
     time:'1 years ago',
     genre:'comedy'
@@ -235,7 +235,7 @@ export const dataInfo=[{
     imageIcon:'https://i.ytimg.com/vi/xcLCe5iBvVc/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDnxWQmOS4mehyG5GS-7gSs-I-lyw',
     video_url:"https://www.youtube.com/embed/xcLCe5iBvVc?si=TpiXZDPunWdYMtBL",
     description:'The Gospel According to Berkeley.',
-    owner:'Aman Singh',
+    owner:'Álex Pina',
     views:'3M Views',
     time:'1 years ago',
     genre:'comedy'
@@ -246,7 +246,7 @@ export const dataInfo=[{
     imageIcon:'https://i.ytimg.com/vi/eNjcAgNu1Ac/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLCHQJRoJegtHwyIvk7O0-35oHFKDw',
     video_url:"https://www.youtube.com/embed/eNjcAgNu1Ac?si=lh_tvi6I3d-F_56L",
     description:'Trump: We speak English here, not Spanish',
-    owner:'Aman Singh',
+    owner:'Álex Pina',
     views:'3M Views',
     time:'1 years ago',
     genre:'news'
